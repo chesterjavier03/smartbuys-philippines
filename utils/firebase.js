cff41f8e4c1c43048eb7793298e3f1b2edda0ce5
Chester Javier <chesterjavier03@duck.com>
2023-04-11 12:12:51 +0800

    SmartBuys Philippines: - Update firebase configuration

--- utils/firebase.js
+++ utils/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDHY-jxEAWSFju85H159Vtiih1Xx914RZo',
-  authDomain: 'smartbuys-88b18.firebaseapp.com',
-  projectId: 'smartbuys-88b18',
-  storageBucket: 'smartbuys-88b18.appspot.com',
-  messagingSenderId: '346710178809',
-  appId: '1:346710178809:web:8b239d2a64035c0dfedb28',
-  measurementId: 'G-B8XB7TME8P',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
