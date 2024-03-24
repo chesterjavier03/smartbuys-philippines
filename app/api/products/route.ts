131d4aa6fe66c15b2f8d82334df04debff7988c9
Chester Javier <chesterjavier03@duck.com>
2024-03-24 15:45:56 +0800

    SmartBuys Philippines 2024: -Add random product result support

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,24 +1,23 @@
 import prisma from '@/prisma/client';
+import { Product } from '@prisma/client';
 import { NextResponse } from "next/server";
 import { cache } from "react";
 
 export const GET = async () => {
-  return NextResponse.json(await fetchProduct());
+  const result = await fetchProduct();
+  return NextResponse.json(shuffleProducts(result));
 }
 
 const fetchProduct = cache(() =>
   prisma.product.findMany({ orderBy: {createdAt: "desc"}})
 );
-// const fetchProduct = async () => {
-//   return shuffleProducts(await prisma.product.findMany({}));
-// }
 
-// const shuffleProducts = (products: Product[]): Product[] => {
-//     const shuffleProducts = [...products];
-//     for (let i = shuffleProducts.length - 1; i > 0; i--) {
-//         const j = Math.floor(Math.random() * (i + 1));
-//         [shuffleProducts[i], shuffleProducts[j]] = [shuffleProducts[j], shuffleProducts[i]];
-//     }
+const shuffleProducts = (products: Product[]): Product[] => {
+    const shuffleProducts = [...products];
+    for (let i = shuffleProducts.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffleProducts[i], shuffleProducts[j]] = [shuffleProducts[j], shuffleProducts[i]];
+    }
 
-//     return shuffleProducts;
-// }
+    return shuffleProducts;
+}
