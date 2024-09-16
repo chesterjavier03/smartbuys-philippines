724973cc7a9f92de964cf3c1e4d8796c659b486c
Chester Javier <chesterjavier03@duck.com>
2024-09-16 18:32:07 +0800

    SmartBuys Philippines 2024: - Use vercel_url for baseUrl - Code clean up

--- lib/base-url.ts
+++ lib/base-url.ts
@@ -1,5 +1,5 @@
 export default function getBaseURL() {
   if (typeof window !== "undefined") return ""
-  if (process.env.VERCEL_URL) return `https://${process.env.DOMAIN_URL}`
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
   return "http://localhost:3000"
 }
