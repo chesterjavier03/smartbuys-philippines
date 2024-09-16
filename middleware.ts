81ed98ba16b81131e46193173c39d3a79d2e41b8
Chester Javier <chesterjavier03@duck.com>
2024-09-16 17:12:10 +0800

    SmartBuys Philippines 2024: - Update matcher in middleware.ts - Code clean up

--- middleware.ts
+++ middleware.ts
@@ -27,10 +27,10 @@ export async function middleware(req: NextRequest) {
 };
 
 export const config = {
-  // matcher: [
-  //   '/dashboard/add-product',
-  //   '/dashboard/products',
-  //   '/dashboard/analytics',
-  // ],
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/dashboard/add-product',
+    '/dashboard/products',
+    '/dashboard/analytics',
+  ],
+  // matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
