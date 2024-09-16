770e36aa1df3d5b8f299870eb0ec693677b2b8ac
Chester Javier <chesterjavier03@duck.com>
2024-09-16 13:43:10 +0800

    SmartBuys Philippines 2024: - Added api/products route for mobile app

---
+++ app/api/products/route.ts
@@ -0,0 +1,27 @@
+import prisma from '@/prisma/client';
+import { Product } from '@prisma/client';
+import { NextResponse } from 'next/server';
+import { cache } from 'react';
+export const revalidate = 1;
+
+export const GET = async () => {
+  const result = await fetchProduct();
+  return NextResponse.json(shuffleProducts(result));
+};
+
+const fetchProduct = cache(() =>
+  prisma?.product.findMany({ orderBy: { createdAt: 'desc' } })
+);
+
+const shuffleProducts = (products: Product[]): Product[] => {
+  const shuffleProducts = [...products];
+  for (let i = shuffleProducts.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffleProducts[i], shuffleProducts[j]] = [
+      shuffleProducts[j],
+      shuffleProducts[i],
+    ];
+  }
+
+  return shuffleProducts;
+};
